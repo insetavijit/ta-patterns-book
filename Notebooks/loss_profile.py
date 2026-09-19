8d0ceed8c1436065361d0392f189139f36f28892
avijit sarkar <insetavijit@users.noreply.github.com>
2026-09-19 13:06:33 +0530

    refactor(notebooks): move loose Python scripts from Notebooks/ to .tmp/

--- Notebooks/loss_profile.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Wrapper runner for Core/ta_patterns_book/loss_profile package."""
-
-from ta_patterns_book.loss_profile.cli import main
-
-if __name__ == "__main__":
-    main()
